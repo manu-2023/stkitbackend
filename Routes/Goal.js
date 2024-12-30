13d107887c055ce3953750d7146f49f5bac7fceb
manu-2023 <manumahadev44@gmail.com>
2024-12-30 10:58:17 +0530

    updated Goal

--- Routes/Goal.js
+++ Routes/Goal.js
@@ -1,7 +1,7 @@
 // Router.js
 import express from 'express';
-import addGoal from '../../backend/Controller/User/addGoal.js'; // Adjust path if needed
-import authenticateToken from '../Middlewear/authenticateToken.js'; // Adjust path as needed
+import {addGoal} from '../../backend/Controller/User/addGoal.js'; // Adjust path if needed
+import {authenticateToken} from '../Middlewear/authenticateToken.js'; // Adjust path as needed
 
 const router = express.Router();
 
