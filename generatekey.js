de6af8a39e07a03ec95afaef63e1ca25c6cdaf5d
manu-2023 <manumahadev44@gmail.com>
2024-12-30 11:35:43 +0530

    Update .gitignore and remove tracked files

--- generatekey.js
+++
@@ -1,8 +0,0 @@
-// generateKey.js
-const crypto = require('crypto');
-
-// Generate a random 32-byte key
-const secretKey = crypto.randomBytes(32).toString('base64');
-
-// Print the key to the console
-console.log('Your JWT Secret Key:', secretKey);
